99282401f6972d17266011e8455d2203c6765c01
José Abraham Sánchez Plazola <derokutojosi@gmail.com>
2022-06-27 17:51:03 -0600

    Agregado Floating Action Button (FAB)

--- RecycleView/app/src/main/java/com/example/recycleview/MainActivity.java
+++ RecycleView/app/src/main/java/com/example/recycleview/MainActivity.java
@@ -8,15 +8,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private RecyclerView.LayoutManager layoutManager;
-
+    private FloatingActionButton fabNuevo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fabNuevo = findViewById(R.id.agregarAlumno);
 
         Aplicacion app = (Aplicacion) getApplication();
         recyclerView = findViewById(R.id.recId);
@@ -31,6 +34,13 @@ public class MainActivity extends AppCompatActivity {
                 int posicion = recyclerView.getChildAdapterPosition(v);
                 String dato = app.getAlumnos().get(posicion).getNombre();
                 Toast.makeText(MainActivity.this, "Se hizo click en: " + dato, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        fabNuevo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(app, "Le pico a nuevo", Toast.LENGTH_SHORT).show();
             }
         });
     }
